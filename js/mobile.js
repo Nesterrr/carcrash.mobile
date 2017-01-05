32e3481d3f565ce17daf29bbf160c25289205316
nesterrr <2626246q@gmail.com>
2017-01-05 18:53:47 +0300

    first commit

---
+++ js/mobile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function(){
+	var moreButton = document.querySelectorAll('.circle');
+	var btn = document.querySelectorAll('.more');
+	var addAnimation = document.querySelectorAll('.to-animate');
+	var xClose = document.querySelectorAll('.close');
+
+/*
+	for(var i = 0; i < btn.length; i++) {
+		btn.addEventListener('click', function() {
+			moreButton[i].classList.add('hidden');
+			addAnimation[i].classList.add('serv-2-anim-block');
+			xClose[i].classList.add('xclose');
+			xClose[i].addEventListener('click', function(evt) {
+				var tgt = evt.target;
+				moreButton[i].classList.remove('hidden');
+				addAnimation[i].classList.remove('serv-1-anim-block');
+				xClose[i].classList.remove('xclose');
+			}, true);
+		}, true);
+	}*/
+
+	
+})();
